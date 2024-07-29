f04d09b3e3cecbb1ab4305d7474598da40983db4
drashtipatell18 <drashti.kalathiyainfotech@gmail.com>
2024-07-29 10:11:26 +0530

    foget password design done

--- resources/views/auth/forgetpass.blade.php
+++ resources/views/auth/forgetpass.blade.php
@@ -1,33 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="limiter">
+    <div class="container">
 
-        <div class="container-login100">
-            <div class="wrap-login100">
-                <form class="login100-form" action="{{ route('forget.password.email') }}" method="POST">
-                    <span class="login100-form-title p-b-43">
-                      Forget Password
-                    </span>
-                    @csrf
+        <div class="card card-w-h">
+            <form action="{{ route('forget.password.email') }}" method="POST">
+                <span class="login100-form-title p-b-43">
+                    Forget Password
+                </span>
+                @csrf
 
-                    <div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-                        <input class="input100" type="text" name="email">
-                        <span class="focus-input100"></span>
-                        <span class="label-input100">Email</span>
-                    </div>
-
-                    <div class="container-login100-form-btn">
-                        <button class="login100-form-btn">
-                            Send
-                        </button>
-                    </div>
-
-                </form>
-
-                <div class="login100-more" style="background-image: url('images/bg-01.jpg');">
+                <div class="label-float">
+                    <input name="email" type='text' id='email' placeholder="">
+                    <label for='email'>Email</label>
+    
+                    @error('email')
+                        <div class="text-danger" style="font-size: small">{{ $message }}</div>
+                    @enderror
                 </div>
-            </div>
+    
+                <div class="container-login100-form-btn">
+                    <button class="login100-form-btn">
+                        Send
+                    </button>
+                </div>
+                <hr class="hr">
+            </form>
         </div>
+    </div>
     </div>
 @endsection
